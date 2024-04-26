a412310594b0c901a52e894601e5fab71e94fc0b
ericgarcia11 <eric.d.r.garcia@gmail.com>
2024-04-26 22:10:35 +0500

    prep assignment 3 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string response = "yes";
+        while (response == "yes"){
+            Random random = new Random();
+            int magic = random.Next(1, 11);
+        
+            int guess = -21321;
+
+            int count = 0;
+
+            while (magic != guess)
+            {   
+                if (guess > magic){
+                    Console.WriteLine("Lower");
+                } else {
+                    Console.WriteLine("Higher");
+                }
+
+                Console.WriteLine("What is your guess?");
+                string input = Console.ReadLine();
+                guess = Convert.ToInt32(input);
+
+                if (guess == magic){
+                    break;
+                } 
+                count += 1;
+            }
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"It took you {count} guesses.");
+            Console.Write("Do you want to continue? ");
+            response = Console.ReadLine();
+        }
     }
 }
