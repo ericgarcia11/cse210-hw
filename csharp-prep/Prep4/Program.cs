d8da7826ecbfee4d91a5b7bf5852987b3b8d372f
ericgarcia11 <eric.d.r.garcia@gmail.com>
2024-04-26 22:36:56 +0500

    a

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Console.WriteLine("Hello Prep4 World!");
+        // List<int> numbers = new List<int>();
+        // List<string> words = new List<string>();
+        // words.Add("phone");
+        // words.Add("keyboard");
+        // words.Add("mouse");
+        // Console.WriteLine(words.Count);
+
+        // ----- Iterating through a List -----
+
+        // foreach (string word in words)
+        // {
+        //     Console.WriteLine(word);
+        // }
+        //  ---- getting the item by their index ----
+        // for (int i = 0; i < words.Count; i++)
+        // {
+        //     Console.WriteLine(words[i]);
+        // }
+
+        List<int> numbers = new List<int>();
+        int number = 1;
+
+        do
+        {
+              Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        } 
+        while (number != 0);{
+            Console.WriteLine("Enter number: ");
+            string input = Console.ReadLine();
+            number = Convert.ToInt32(input);
+            numbers.Add(number);
+        }
+        Console.WriteLine($"Your list is {numbers}");
+
+
+        
+
+
     }
 }
