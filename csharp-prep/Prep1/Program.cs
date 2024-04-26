87c8cfd409aad86fa8c645abbacf33a230e3515c
ericgarcia11 <eric.d.r.garcia@gmail.com>
2024-04-26 21:08:17 +0500

    .

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -9,6 +9,6 @@ class Program
         string fname = Console.ReadLine();
         Console.WriteLine("What is your last name?");
         string lname = Console.ReadLine();
-        Console.WriteLine($"Your name is {fname}, {fname} {lname}");
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
     }
 }
