cc156daf1e80a09ee04cfad682c6254aaae43588
ericgarcia11 <eric.d.r.garcia@gmail.com>
2024-04-30 22:17:37 +0500

    a

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Security.Principal;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._companyName = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2014;
+        job1._endYear = 2022;
+        Console.WriteLine(job1._companyName);
+
+        Job job2 = new Job();
+        job2._companyName = "Apple";
+        job2._jobTitle = "Support";
+        job2._startYear = 2001;
+        job2._endYear = 2023;
+        Console.WriteLine(job2._companyName);
+
     }
 }
