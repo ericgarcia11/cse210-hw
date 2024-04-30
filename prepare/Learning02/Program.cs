3baa79d756f77bc83a95c20087c1c231984f8fb7
ericgarcia11 <eric.d.r.garcia@gmail.com>
2024-04-30 22:21:43 +0500

    a

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -10,14 +10,16 @@ class Program
         job1._jobTitle = "Software Engineer";
         job1._startYear = 2014;
         job1._endYear = 2022;
-        Console.WriteLine(job1._companyName);
+        // Console.WriteLine(job1._companyName);
+        job1.ShowJobInformation();
 
         Job job2 = new Job();
         job2._companyName = "Apple";
         job2._jobTitle = "Support";
         job2._startYear = 2001;
         job2._endYear = 2023;
-        Console.WriteLine(job2._companyName);
+        // Console.WriteLine(job2._companyName);
+        job1.ShowJobInformation();
 
     }
 }
