1942309630782641d4b651cf2ad31dffa430784a
ericgarcia11 <eric.d.r.garcia@gmail.com>
2024-05-01 16:21:19 +0500

    group program structure

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        int response = 0;
+        while (response != 5)
+        {
+            Console.WriteLine("Please, select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.WriteLine("What would you like to do?");
+            string input = Console.ReadLine();
+            response = Convert.ToInt32(input); 
+            if (response == 5){
+                break;
+            }
+            if (response == 1){
+                Write day1 = new Write();
+                Console.WriteLine("If I had one thing I could over today, what would it be?");
+                day1._questionOne = Console.ReadLine();
+                Console.WriteLine("What was the best part of my day?");
+                day1._questionTwo = Console.ReadLine();
+                Console.WriteLine("If I had one thing I could over today, what would it be?");
+                day1._questionThree = Console.ReadLine();
+            }
+
+
+        }
     }
 }
---
+++ prove/Develop02/Write.cs
@@ -0,0 +1,15 @@
+public class Write
+{
+     public string _questionOne;
+     public string _questionTwo;
+     public string _questionThree;
+
+     public Write()
+    {
+    }
+
+    public void ShowJobInformation()
+    {
+            Console.WriteLine($"");
+    }
+}
