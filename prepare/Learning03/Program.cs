45e63c5d05323db24787d05206b3bdb8cf0626f1
Eric Garcia <133874193+ericgarcia11@users.noreply.github.com>
2024-05-09 23:35:53 -0300

    a

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,68 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        string fractionStr1 = fraction1.GetFractionString();
+        double fractionDec1 = fraction1.GetDecimalValue();
+        Fraction fraction2 = new Fraction(5);
+        string fractionStr2 = fraction2.GetFractionString();
+        double fractionDec2 = fraction2.GetDecimalValue();
+        Fraction fraction3 = new Fraction(3,4);
+        string fractionStr3 = fraction3.GetFractionString();
+        double fractionDec3 = fraction3.GetDecimalValue();
+    }
+}
+
+class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction (){
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction (int numerator){
+        _top = numerator;
+        _bottom = 1;
+    }
+
+    public Fraction (int numerator, int denominator){
+        _top = numerator;
+        _bottom = 1;
+    }
+
+     public int GetNumerator()
+    {
+        return _top;
+    }
+
+    public int GetDenominator()
+    {
+        return _bottom;
+    }
+
+    public void SetNumerator(int numerator)
+    {
+        _top = numerator;
+    }
+
+    public void SetNumeratorAndDenominator(int numerator, int denominator)
+    {
+        _top = numerator;
+        _bottom = denominator;
+    }
+
+    public string GetFractionString()
+    {
+        string fractionString = ($"{_top}/{_bottom}");
+        return fractionString;
+    }
+
+    public double GetDecimalValue()
+    {
+        double dividing = _top / _bottom; 
+        return dividing;
     }
 }
