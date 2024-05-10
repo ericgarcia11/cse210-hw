deff042bbb4781d4d9a3a021586eab96e137992a
Eric Garcia <133874193+ericgarcia11@users.noreply.github.com>
2024-05-09 23:38:42 -0300

    done

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -6,13 +6,19 @@ class Program
     {
         Fraction fraction1 = new Fraction();
         string fractionStr1 = fraction1.GetFractionString();
+        Console.WriteLine(fractionStr1);
         double fractionDec1 = fraction1.GetDecimalValue();
+        Console.WriteLine(fractionDec1);
         Fraction fraction2 = new Fraction(5);
         string fractionStr2 = fraction2.GetFractionString();
+        Console.WriteLine(fractionStr2);
         double fractionDec2 = fraction2.GetDecimalValue();
+        Console.WriteLine(fractionDec2);
         Fraction fraction3 = new Fraction(3,4);
         string fractionStr3 = fraction3.GetFractionString();
+        Console.WriteLine(fractionStr3);
         double fractionDec3 = fraction3.GetDecimalValue();
+        Console.WriteLine(fractionDec3);
     }
 }
 
